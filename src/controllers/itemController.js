af98fe9ffe148b3e76895a4f078673ad950271a3
NKP1229 <nikhilkpatel1229@gmail.com>
2025-04-28 10:33:03 -0400

    created empty functions in itemController

--- src/controllers/itemController.js
+++ src/controllers/itemController.js
@@ -1,1 +1,5 @@
 const { router, bcrypt, prisma, jwt } = require("../common/common");
+
+const getAllItems = async (req, res, next) => {};
+const getItem = async (req, res, next) => {};
+const getItemReviews = async (req, res, next) => {};
